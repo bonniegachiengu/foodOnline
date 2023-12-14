259eefa04f50fba07d66df26dfeb8a51839f33ad
Bonnie Gachiengu <bonniegachiengu@gmail.com>
2023-12-14 21:42:49 +0300

    breakdown of templates to add base.html

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 
 
 class UserManager(BaseUserManager):
